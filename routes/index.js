29219b25d69cc14be0051cc573ebade6f580fe0e
Daniele <tortora87@gmail.com>
2020-08-18 12:58:17 +0200

    login route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ import { encode } from "../middlewares/jwt.js";
 
 const router = express.Router();
 
-router.post("/login/:userId", encode, (req, res, next) => {});
+router.post("/login/:userId", encode, (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    authorization: req.authToken,
+  });
+});
 
 export default router;
